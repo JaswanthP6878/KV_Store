34deec79c12ec8b5fa5443da874476224313934b
JaswanthP6878 <pinnepujaswanth0@gmail.com>
2025-09-10 17:20:57 +0530

    prek installation

--- src/store/mod.rs
+++ src/store/mod.rs
@@ -1,4 +1,9 @@
+use std::collections::BTreeMap;
+
 use openraft::BasicNode;
+use openraft::LogId;
+use openraft::Node;
+use openraft::StoredMembership;
 use serde::Deserialize;
 use serde::Serialize;
 
@@ -22,7 +27,25 @@ pub struct Response {
 // how are logs stored and how can the data be retrived
 pub type LogStore = memlog::LogStore<TypeConfig>;
 
+
+// stored SnapShot is used to send the copy to other raft instances
+// or restore the state
+#[derive(Debug)]
 pub struct StoredSnapshot {
     pub meta: SnapshotMeta<NodeId, BasicNode>,
+    // the data at a given snapshot moment
     pub data: Vec<u8>
 }
+
+#[derive(Serialize, Deserialize, Debug,Default, Clone)]
+pub struct StateMachineData {
+    pub last_applied_log: Option<LogId<NodeId>>,
+ 
+    pub last_membership_log: StoredMembership<NodeId, BasicNode>,
+
+    pub data: BTreeMap<String, String>,
+}
+
+
+
+
